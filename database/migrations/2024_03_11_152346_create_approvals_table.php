36cf95c3f939fc706d68840f43484dd3adf0b1fd
suhairi <suhairi81@gmail.com>
2024-03-17 10:18:53 +0800

    20240317 upgrade to laravel v11

--- database/migrations/2024_03_11_152346_create_approvals_table.php
+++ database/migrations/2024_03_11_152346_create_approvals_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('approvals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('vehiclebooking_id');
             $table->foreignId('user_id'); // approver
             $table->string('status'); // approve or disapprove or pending
             $table->timestamps();
